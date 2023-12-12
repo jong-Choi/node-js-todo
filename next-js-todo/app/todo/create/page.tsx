1c071e24682834de69fa1eed4d735d43f3db2008
jonghyunChoi <bluecoolgod80@gmail.com>
2023-12-12 18:17:12 +0900

    [feat] next js 기본 앱 제작

--- next-js-todo/app/todo/create/page.tsx
+++ next-js-todo/app/todo/create/page.tsx
@@ -1,36 +1,12 @@
-"use client";
-import createUsers from "@/lib/createUsers";
+import CreateForm from "@/components/create/CreateForm";
 import getUsers from "@/lib/getUsers";
-import { FormEvent, useState } from "react";
 
-const UserCreate = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    createUsers({ username, email }).then((res) => {
-      setUsername("");
-      setEmail("");
-    });
-  };
-  const users = getUsers();
+const UserCreate = async () => {
+  const users = await getUsers();
 
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          placeholder="이름"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          placeholder="이메일"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <button type="submit">사용자 생성하기</button>
-      </form>
+      <CreateForm />
       {JSON.stringify(users)}
     </div>
   );
